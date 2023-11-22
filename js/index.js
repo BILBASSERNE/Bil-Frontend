1e5816c5f5b97976a99d11aee74377ffddebc1e4
navidmirzad <navi0221@stud.kea.dk>
2023-11-22 13:33:16 +0100

    CreateAccount works now lol

--- js/index.js
+++ js/index.js
@@ -1,3 +1,78 @@
-console.log("i am in index")
+document.addEventListener('DOMContentLoaded', function () {
+    var modal = document.getElementById("myModal");
+    var btn = document.getElementById("myBtn");
+    var span = document.getElementsByClassName("close")[0];
 
-//Test af André
+    btn.onclick = function () {
+        modal.style.display = "block";
+    }
+
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+    const createAccountUrl = "http://localhost:8080/createAccount";
+
+    const username = document.getElementById("username");
+    const password = document.getElementById("password");
+    const firstname = document.getElementById("firstname");
+    const lastname = document.getElementById("lastname");
+    const city = document.getElementById("city");
+    const phoneNumber = document.getElementById("phoneNumber");
+    const email = document.getElementById("email");
+    const submitNewAccount = document.getElementById("submitAccount")
+
+    submitNewAccount.addEventListener("click", async function (event) {
+        event.preventDefault(); // Prevent the form from submitting normally
+
+        const newAccount = {
+            username: username.value,
+            password: password.value,
+            firstName: firstname.value,
+            lastName: lastname.value,
+            city: city.value,
+            phoneNumber: phoneNumber.value,
+            email: email.value
+        };
+
+        const postUrl = "http://localhost:8080/createAccount";
+
+
+        try {
+            const response = await postObjectAsJson(postUrl, newAccount, "POST");
+            console.log("inside postCustomer");
+
+            if (response.ok) {
+                alert("Account Created!");
+            } else {
+                alert("Error creating account");
+            }
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    });
+
+
+    async function postObjectAsJson(url, object, httpVerbum) {
+        const objectAsJsonString = JSON.stringify(object)
+        console.log(objectAsJsonString)
+        const fetchOptions = {
+            method: httpVerbum,
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: objectAsJsonString
+        }
+        const response = await fetch(url, fetchOptions)
+        console.log(response)
+        return response
+    }
+
+
+});
