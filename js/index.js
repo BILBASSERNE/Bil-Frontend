60850744c70666db595d9f5da71aea6c4dda65f9
Enes Kocer <enes0539@stud.kea.dk>
2023-11-23 11:47:56 +0100

    auto generate car cards

--- js/index.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("filterToggle").addEventListener("click", function() {
-    var filterContent = document.getElementById("filterContent");
-    filterContent.style.display = filterContent.style.display === "none" ? "block" : "none";
-});
---
+++ js/modulejson.js
@@ -0,0 +1,20 @@
+async function postObjectAsJson(url, object, httpVerbum) {
+    const objectAsJsonString = JSON.stringify(object)
+    console.log(objectAsJsonString)
+    const fetchOptions = {
+        method: httpVerbum,
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: objectAsJsonString
+    }
+    const response = await fetch(url, fetchOptions)
+    return response
+}
+
+function fetchAnyUrl(url) {
+    return  fetch(url).then(response => response.json());
+}
+
+
+export {postObjectAsJson, fetchAnyUrl}
