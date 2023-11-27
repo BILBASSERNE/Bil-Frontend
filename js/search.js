b13d2b621ec5aa6d9c2dd8f677e56952e35569bb
Youssef Talbi <112815657+youssef12314@users.noreply.github.com>
2023-11-27 10:06:59 +0100

    Responseentity and "no cars found" function

--- js/search.js
+++ js/search.js
@@ -1,8 +1,21 @@
 function search() {
     var keyword = document.getElementById("searchInput").value;
     fetch('http://localhost:8080/cars?keyword=' + keyword)
-        .then(response => response.json())
-        .then(data => displayResults(data));
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("No cars found");
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data.length > 0) {
+                displayResults(data);
+                alert("Search function works!");
+            } else {
+                throw new Error("No cars found");
+            }
+        })
+        .catch(error => displayErrorMessage(error.message));
 }
 
 function displayResults(results) {
@@ -14,4 +27,9 @@ function displayResults(results) {
         resultDiv.textContent = result.name; // Display the name of the car
         resultsDiv.appendChild(resultDiv);
     });
+}
+
+function displayErrorMessage(message) {
+    var resultsDiv = document.getElementById("searchResults");
+    resultsDiv.innerHTML = "<div>" + message + "</div>";
 }
