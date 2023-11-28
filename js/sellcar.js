762fcdf664a0fe4ece797048c560db6e68591b68
Enes Kocer <enes0539@stud.kea.dk>
2023-11-28 09:49:10 +0100

    rentcar works again

--- js/sellcar.js
+++ js/sellcar.js
@@ -142,29 +142,3 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
 });
-
-/*class ModalHandler {
-    constructor(modalId, triggerBtnId) {
-        this.modal = document.getElementById(modalId);
-        this.triggerBtn = document.getElementById(triggerBtnId);
-
-        // Bind methods to the instance to maintain the correct 'this' context
-        this.showModal = this.showModal.bind(this);
-        this.hideModal = this.hideModal.bind(this);
-
-        // Attach event listeners
-        this.attachEventListeners();
-    }
-
-    attachEventListeners() {
-        this.triggerBtn.addEventListener('click', this.showModal);
-
-        // Add event listener for closing the modal (if needed)
-        this.modal.addEventListener('click', this.hideModal);
-    }
-
-
-
-
-
-
